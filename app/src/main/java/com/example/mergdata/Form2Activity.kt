4e0646d083ccea680b9669d356bf4e94f370a59d
Steven Kofi Nnuro <48254031+stevennuro991@users.noreply.github.com>
2023-02-21 07:41:55 +0000

    Completed Assignment

---
+++ app/src/main/java/com/example/mergdata/Form2Activity.kt
@@ -0,0 +1,11 @@
+package com.example.mergdata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Form2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form2)
+    }
+}
---
+++ app/src/main/java/com/example/mergdata/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.mergdata
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mergdata.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var  binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnRegister.setOnClickListener{
+            startActivity(Intent(this,FormActivity::class.java))
+        }
+        binding.btnRegister2.setOnClickListener {
+            startActivity(Intent(this,Form2Activity::class.java))
+        }
+    }
+}
